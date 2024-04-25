75ccb36fb0009bc40d2324c272571cf3b43ddaf1
OxanaFedorova <100290350+OxanaFedorova@users.noreply.github.com>
2024-04-25 23:01:55 +0300

    update 25.04 16:32

--- main.py
+++ main.py
@@ -1,109 +1,9 @@
-# import json
-#
-# from flask_login import LoginManager, logout_user, login_required, current_user, login_user
-# from flask import render_template, redirect, make_response, jsonify, url_for
-# from flask import Flask
-# from data.users import User
-# from forms.loginform import LoginForm
-# from forms.user import RegisterForm
-# from data import db_session
-#
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-#
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-#
-#
-# @login_manager.user_loader
-# def load_user(user_id):
-#     db_sess = db_session.create_session()
-#     return db_sess.query(User).get(user_id)
-#
-#
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     news = []
-#     if current_user.is_authenticated:
-#         with open(url_for('static', filename='json/json.json')[1:], 'rt', encoding='utf8') as f:
-#             news = json.loads(f.read())
-#         return render_template("index.html", news=news['news'])
-#     return render_template("index.html", news=news)
-#
-#
-# @app.route('/register', methods=['GET', 'POST'])
-# def reqister():
-#     form = RegisterForm()
-#     if form.validate_on_submit():
-#         if form.password.data != form.password_again.data:
-#             return render_template('register.html', title='Регистрация',
-#                                    form=form,
-#                                    message="Пароли не совпадают")
-#         db_sess = db_session.create_session()
-#         if db_sess.query(User).filter(User.email == form.email.data).first():
-#             return render_template('register.html', title='Регистрация',
-#                                    form=form,
-#                                    message="Такой пользователь уже есть")
-#         user = User(
-#             name=form.name.data,
-#             email=form.email.data,
-#             about=form.about.data
-#         )
-#         user.set_password(form.password.data)
-#         db_sess.add(user)
-#         db_sess.commit()
-#         return redirect('/login')
-#     return render_template('register.html', title='Регистрация', form=form)
-#
-#
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         db_sess = db_session.create_session()
-#         user = db_sess.query(User).filter(User.email == form.email.data).first()
-#         if user and user.check_password(form.password.data):
-#             login_user(user, remember=form.remember_me.data)
-#             return redirect("/")
-#         return render_template('login.html',
-#                                message="Неправильный логин или пароль",
-#                                form=form)
-#     return render_template('login.html', title='Авторизация', form=form)
-#
-#
-# @app.route('/profile', methods=['GET', 'POST'])
-# def profile():
-#
-#     return render_template('profile.html', title='Профиль', form=form)
-#
-#
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect("/")
-#
-#
-# @app.errorhandler(404)
-# def not_found(error):
-#     print(error)
-#     return make_response(jsonify({'error': 'not found'}), 404)
-#
-#
-# @app.errorhandler(400)
-# def bad_request(_):
-#     return make_response(jsonify({'error': 'Bad Request'}), 400)
-#
-#
-# if __name__ == '__main__':
-#     db_session.global_init("blogs.db")
-#     app.run(port=8080, host='127.0.0.1')
+import os
+import shutil
 
-
-import json
 from flask_login import LoginManager, logout_user, login_required, current_user, login_user
 from flask import Flask, render_template, url_for, redirect, request, make_response, session, abort, jsonify
+from werkzeug.utils import secure_filename
 
 import news_resources
 from data.news import News
@@ -113,8 +13,7 @@ from forms.news import NewsForm
 from forms.user import RegisterForm
 from forms.profile import ProfileForm
 from data import db_session, news_api
-from flask_restful import reqparse, abort, Api, Resource
-
+from flask_restful import abort, Api
 
 MAX_FILE_SIZE = 1024 * 1024 + 1
 
@@ -207,11 +106,7 @@ def profile(id):
             form.about.data = user.about
         else:
             abort(404)
-    elif request.method == 'POST':
-        if request.files['file']:
-            db_sess = db_session.create_session()
-            user = db_sess.query(User).filter(User.id == id).first()
-            user.photo = request.files['file']
+
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.id == id).first()
@@ -220,7 +115,29 @@ def profile(id):
             user.name = form.name.data
             user.about = form.about.data
             db_sess.commit()
-            return redirect('/')
+
+            if 'file' not in request.files:  # файл нету
+                return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':  # файл не выбран пользолвателем
+                return redirect(request.url)
+
+            if file:
+                if os.path.exists(f'static/img/{id}'):
+                    shutil.rmtree(f'static/img/{id}')
+
+                child_folder = os.path.join('static/img/', str(id))
+                os.makedirs(child_folder)
+
+                filename = secure_filename(file.filename)
+                file.save(f'static/img/{id}/{filename}')
+                user.photo = f'img/{id}/{filename}'
+                db_sess.commit()
+                return render_template('profile.html', form=form, filename=filename)
+            else:
+                return redirect(request.url)
+
+            db_sess.commit()
         else:
             abort(404)
     return render_template('profile.html', form=form)
@@ -364,7 +281,7 @@ def session_test():
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': error}), 404)
+    return make_response(jsonify({'error': '404'}), 404)
 
 
 @app.errorhandler(400)
